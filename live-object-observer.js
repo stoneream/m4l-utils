9c233c6b31dac0e0c40b39b2b8c397032d868246
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-11-30 03:08:06 +0900

    LiveObjectObserverクラスを追加

---
+++ live-object-observer.js
@@ -0,0 +1,47 @@
+class LiveObjectObserver {
+  constructor(path, property, handler) {
+    this.logger = new Logger("LiveObjectObserver");
+
+    this.initialized = false;
+    this.detectedPropertySet = false;
+    this.detectedFirst = false;
+
+    this.api = new LiveAPI(() => this._callback(), path);
+
+    this.property = property;
+    this.handler = handler;
+  }
+
+  _callback() {
+    // ハンドラーがセットされていない場合は処理をスキップ
+    if (!this.handler) {
+      return;
+    }
+
+    // インスタンス生成直後のコールバックをスキップ
+    if (!this.initialized) {
+      this.logger.info("LiveAPI initialized");
+      this.initialized = true;
+
+      this.api.property = this.property;
+      logger.info("Set property", { property: this.property });
+      return;
+    }
+
+    // プロパティセット直後のコールバックをスキップ
+    if (!this.detectedPropertySet) {
+      this.logger.info("Detected property set");
+      this.detectedPropertySet = true;
+      return;
+    }
+
+    // 最初の変更通知をスキップ
+    if (!this.detectedFirst) {
+      this.logger.info("Detected first change notification");
+      this.detectedFirst = true;
+      return;
+    }
+
+    this.handler();
+  }
+}
--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-include("logger.js");
+include("header.js");
 
 const logger = new Logger("main");
+let trackAPI = null;
+
+function trackChangedHandler() {
+  logger.info("Tracks changed");
+}
 
 function bang() {
   logger.info("Device initialization complete!");
+  trackAPI = new LiveObjectObserver("live_set", "tracks", trackChangedHandler);
 }
