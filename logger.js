06cbafdc0a95a39ce33ce5f04a7c72aa5f1f8c81
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-11-29 10:24:16 +0900

    Loggerクラスを追加

---
+++ logger.js
@@ -0,0 +1,66 @@
+class Logger {
+  constructor(name) {
+    this.name = name;
+  }
+
+  lnPost(obj) {
+    if (obj) {
+      post(obj + "\n");
+    }
+  }
+
+  /**
+   * key=value の形式で引数をフォーマットする
+   * @param  {...any} args
+   *
+   * @returns {string[]}
+   */
+  formatKeyValue(...args) {
+    return args.map((arg) => {
+      if (typeof arg === "object" && arg !== null) {
+        return Object.entries(arg)
+          .map(([key, value]) => `${key}=${value}`)
+          .join(", ");
+      } else {
+        return String(arg);
+      }
+    });
+  }
+
+  /**
+   * ログメッセージをフォーマットする
+   * @param {string} level - ログレベル
+   * @param {string} message - メッセージ
+   * @param  {...any} args - 追加の引数
+   *
+   * @returns {string}
+   */
+  format(level, message, ...args) {
+    const timestamp = new Date().toISOString();
+
+    const formattedArgs = () => {
+      if (args && args.length > 0) {
+        // [key1=value1, key2=value2, ...] の形式で引数をフォーマット
+        return " [" + this.formatKeyValue(...args).join(", ") + "]";
+      } else {
+        return "";
+      }
+    };
+
+    return `${timestamp} [${level}] ${
+      this.name
+    }: ${message} ${formattedArgs()}`;
+  }
+
+  info(message, ...args) {
+    this.lnPost(this.format("INFO", message, ...args));
+  }
+
+  warn(message, ...args) {
+    this.lnPost(this.format("WARN", message, ...args));
+  }
+
+  error(message, ...args) {
+    this.lnPost(this.format("ERROR", message, ...args));
+  }
+}
